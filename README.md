fe1f590864bdeeaeab805f73acb47bfcd381e89e
Giuseppe Catalano <giuseppe.catalano@eng.it>
2021-03-20 10:56:12 +0100

    Release of 2nd week assignment

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Classify Customer Feedback with Knative Services on Kubernetes
 ## Test URL
 * export INGRESS_IP_ADDRESS=$(kubectl --namespace istio-system get service istio-ingressgateway -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
 * curl -d '{"feedback":"All ok!"}' -H "Content-Type:application/json" -X POST http://${INGRESS_IP_ADDRESS}
+
+# 2) Accept and Store Feedback with Google's NoSQL Firestore
+* export INGRESS_IP_ADDRESS=$(kubectl --namespace istio-system get service istio-ingressgateway -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+* curl -d '{"feedback":"All ok!"}' -H "Content-Type:application/json" -X POST http://${INGRESS_IP_ADDRESS}
--- gcp-env/setup_gke_istio.sh
+++ gcp-env/setup_gke_istio.sh
@@ -4,10 +4,13 @@
 gcloud config set project integral-bliss-306817
 gcloud config set compute/zone europe-west4-c
 
+#gcloud services enable cloudresourcemanager.googleapis.com pubsub.googleapis.com
+
 gcloud container clusters create customer-feedback \
     --cluster-version=1.18.12-gke.1210 \
     --machine-type=n1-standard-2 \
-    --num-nodes=4
+    --num-nodes=4 \
+	--workload-pool=integral-bliss-306817.svc.id.goog
 	
 kubectl apply --filename https://github.com/knative/serving/releases/download/v0.21.0/serving-crds.yaml
 kubectl apply --filename https://github.com/knative/serving/releases/download/v0.21.0/serving-core.yaml
@@ -17,4 +20,9 @@ kubectl apply --filename https://github.com/knative/serving/releases/download/v0
 
 gcloud pubsub topics create feedback-created
 gcloud pubsub topics create feedback-classified
+
+kubectl apply -f ../trigger-func/trigger-func.yaml
+kubectl apply -f ../trigger-func/trigger-func-istio.yaml
+
+kubectl create secret generic pubsub-key --from-file=key.json=../trigger-func/pubsub_serviceaccount.json
 
